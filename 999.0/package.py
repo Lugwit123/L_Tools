1c737f4e78ae57fe807e9cfccdc51b6800743794
wb.fengqingqing <wb.fengqingqing@mesg.corp.netease.com>
2026-06-28 11:05:08 +0800

    Update L_Tools package.py
    Update package.py to fix some issues and add new features.

--- 999.0/package.py
+++ 999.0/package.py
@@ -5,7 +5,7 @@ version = "999.0"
 description = "Lugwit system tools - window management, admin utilities"
 authors = ["Lugwit Team"]
 
-requires = ["python-3.12+<3.13"]
+requires = ["python-3.12+<3.13", "pyside6", "Lugwit_Module", "pywin32", "qtpy", "l_qt_wgt_lib"]
 
 build_command = False
 cachable = True
@@ -16,3 +16,5 @@ def commands():
     # sys_tool 目录下的模块直接可 import（l_window, l_admin, l_window_control 等）
     env.PYTHONPATH.prepend("{root}/sys_tool")
     env.L_TOOLS_ROOT = "{root}"
+    alias("set_disk_link", "python {root}/sys_tool/setDiskLink/setDiskLink.py")
+    alias("set_env_var", "python {root}/sys_tool/setEnvVar.py")
